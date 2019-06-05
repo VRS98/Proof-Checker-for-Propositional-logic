5a69a520a1b142e0353f9699c729b7227bc4e54a
Vishakh Shah <shah.vishakh.98@gmail.com>
2019-06-05 14:21:34 +0530

    Add files via upload

---
+++ mainpage.cpp
@@ -0,0 +1,18 @@
+/**
+\mainpage Description
+\section authors Authors
+* Team:
+* 1. Mehul Agrawal: 2017A7PS0054H
+* 2. Shah Vishakh Rakesh: 2017A7PS1445H
+* 3. Abhishek Dhanwani: 2017A7PS0161H
+\section part1 Part-1
+* Forms Postfix notation from Infix notation input.
+* Forms Parse Tree of Postfix notation and returns Inorder traversal of the tree back.
+\section part2 Part-2
+* Checks the validity of the proof of a given sequent of Propositional Logic by employing the following rules of natural deduction:
+* 1. AND Introduction
+* 2. AND Elimination
+* 3. OR Introduction
+* 4. IMPLICATION Elimination
+* 5. MODUS TOLLENS
+*/
